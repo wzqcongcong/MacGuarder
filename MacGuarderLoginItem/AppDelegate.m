becd0c9ab254ec95dc915779578971881bdd523a
wzqcongcong <wzqcongcong@sina.com>
2015-09-16 16:21:02 +0800

    delay to terminate login item

--- MacGuarderLoginItem/AppDelegate.m
+++ MacGuarderLoginItem/AppDelegate.m
@@ -33,7 +33,9 @@
                                                     launchIdentifier:NULL];
     }
 
-    [[NSApplication sharedApplication] terminate:self];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[NSApplication sharedApplication] terminate:self];
+    });
 }
 
 - (NSString *)wrapperAppBundleID
